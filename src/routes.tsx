456e0f26a39247f3042edcd74cabc988365959aa
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-12-10 11:44:58 -0300

    feat: add base layout for pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layouts/app'
 import { Dashboard } from './pages/dashboard'
 import { Services } from './pages/services/services'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />,
-  },
-  {
-    path: '/services',
-    element: <Services />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: '/services',
+        element: <Services />,
+      },
+    ],
   },
 ])
