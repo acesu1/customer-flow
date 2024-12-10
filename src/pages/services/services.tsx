05569a957b95da99b700700447623d6b627893ad
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-12-10 12:23:00 -0300

    feat: add service table

--- src/pages/services/services.tsx
+++ src/pages/services/services.tsx
@@ -1,13 +1,91 @@
+import { Button } from '@/components/ui/button'
 import { ServiceTableFilters } from './service-table-filters'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { ArrowRight, Search, X } from 'lucide-react'
 
 export function Services() {
   return (
     (
       <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold tracking-tight">Serviços</h1>
+        <h1 className="text-3xl font-bold">Serviços</h1>
 
         <div className="space-y-2.5">
           <ServiceTableFilters />
+          <div className="border rounded-md">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]" />
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do serviço</TableHead>
+                  <TableHead className="w-[164px]" />
+                  <TableHead className="w-[132px]" />
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return (
+                    <TableRow key={i}>
+                      <TableCell>
+                        <Button variant="outline" size="xs">
+                          <Search className="h-3 w-3" />
+                          <span className="sr-only">Detalhes do serviço</span>
+                        </Button>
+                      </TableCell>
+
+                      <TableCell className="text-xs font-medium">
+                        ylph18rfdzw3qrpkde2ta4cn
+                      </TableCell>
+
+                      <TableCell className="text-muted-foreground">
+                        há 4 dias
+                      </TableCell>
+
+                      <TableCell>
+                        <div className="flex items-center gap-2">
+                          <span className="size-2 rounded-full bg-amber-500" />
+                          <span className="font-medium text-muted-foreground">
+                            Pendente
+                          </span>
+                        </div>
+                      </TableCell>
+
+                      <TableCell className="font-medium">
+                        Richard Quitzon
+                      </TableCell>
+                      <TableCell>
+                        R$ 20,78
+                      </TableCell>
+
+                      <TableCell>
+                        <Button variant="outline" size="xs">
+                          <ArrowRight className="mr-2 size-4" />
+                          Aprovar
+                        </Button>
+                      </TableCell>
+
+                      <TableCell>
+                        <Button variant="ghost" size="xs">
+                          <X className="mr-2 size-4" />
+                          Cancelar
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       </div>
     )
